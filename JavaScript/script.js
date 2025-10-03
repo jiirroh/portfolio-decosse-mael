a9737ebdd0f940f3ab22e259dc752d1a91bdfb5b
jiirroh <mael.decosse@gmail.com>
2025-10-03 09:23:05 +0000

    fix vitesse script ecriture

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,7 +4,7 @@ function typeWriter() {
   if (i < text.length) {
     document.getElementById("typing").innerHTML += text.charAt(i);
     i++;
-    setTimeout(typeWriter, 50);
+    setTimeout(typeWriter, 60);
   }
 }
 window.onload = typeWriter;
