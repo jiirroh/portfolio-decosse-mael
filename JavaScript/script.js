5e3bd47a2ead00f24b6d764bc0b9b9367c86ca26
jiirroh <mael.decosse@gmail.com>
2025-10-03 09:30:39 +0000

    reglage vitesse script ecriture

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,7 +4,7 @@ function typeWriter() {
   if (i < text.length) {
     document.getElementById("typing").innerHTML += text.charAt(i);
     i++;
-    setTimeout(typeWriter, 60);
+    setTimeout(typeWriter, 100);
   }
 }
 window.onload = typeWriter;
