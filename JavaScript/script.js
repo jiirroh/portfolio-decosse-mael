586d31d3c4a72cec5ee3de525f7ffc50260b4bf2
jiirroh <mael.decosse@gmail.com>
2025-10-03 09:16:27 +0000

    mise ne place script écriture

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+const text = "Bienvenue sur mon portfolio ! Découvrez mon CV, mes projets et comment me contacter.";
+let i = 0;
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 50);
+  }
+}
+window.onload = typeWriter;
